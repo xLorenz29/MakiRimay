ab0b3916dc37a09408e8d63dbb1cee0414f7c0e7
xLorenz29 <renzo.29.lp@gmail.com>
2025-11-22 08:44:22 -0500

    Regresando a DO

--- app/back/server.py
+++ app/back/server.py
@@ -42,4 +42,5 @@ def predict():
     return jsonify({'prediction': label})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
